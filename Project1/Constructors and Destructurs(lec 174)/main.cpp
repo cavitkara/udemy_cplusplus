2cc50d5979b399f448cc8d404664ae11c1c419df
Win7 <Win7@WIN7-BILGISAYAR>
2018-12-03 14:50:05 +0200

    Passing Arguments to Base Class constructor

---
+++ Project1/Constructors and Destructurs(lec 174)/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+	class Base {
+
+	public:
+
+		int value;
+
+		Base() { cout << "Base no args constructor" << endl; }
+		Base(int x) :value{x} { cout << "Base constructor" << endl; }
+		~Base() { cout << "Base destructor\n"; }
+	};
+
+	class Derived :public Base {
+		using Base::Base;
+	private:
+		int double_value;
+	public:
+		Derived() { cout << "Derived no args constructor\n"; }
+//		Derived(int x) : double_value{ x * 2 } { cout << "Derived constructor\n"; }
+		~Derived() { cout<< "Derived destructor\n"; }
+	};
+
+		int main() {
+
+//			Base b;
+			Derived d{1000};
+
+
+//			while (1);
+			return 0;
+		}
---
+++ Project1/Passing Args to Base Class Const/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+/*It is shown  a way that to invoke both base class constructor and 
+Derived class constructor with parameter
+*/
+
+using namespace std;
+
+class Base {
+
+private:
+	int value;
+public:
+
+	Base() : value{0} {cout << "Base no args conts\n"; }
+	Base(int x) : value{ x } { cout<<"Base overloded const\n"; }
+};
+
+
+class Derived : public Base {
+
+	int double_value;
+public:
+	Derived() :Base(), double_value{ 0 } { cout<<"Derived no args const\n"; }
+	Derived(int x) : Base(x), double_value{ x * 2 } { cout << "Derived overloaded const\n"; }
+};
+
+
+void main() {
+
+	Derived d{1000};
+
+}
